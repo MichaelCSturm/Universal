48e09e32f9a061b0dfe505fafb160b4b018905a0
Michael Sturm <michael.c.sturm@gmail.com>
2025-04-26 15:13:53 -0500

    animated light and added looks to pose mode

---
+++ Assets/fake_light_script.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fake_light_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
