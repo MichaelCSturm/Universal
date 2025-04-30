179f13c67865ae502a4f3cc0fc40680af1761e78
Michael Sturm <michael.c.sturm@gmail.com>
2025-04-30 11:44:44 -0500

    ahskdjhaskjdh

---
+++ Assets/ModelGrass.cs
@@ -0,0 +1,181 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelGrass : MonoBehaviour {
+    public int resolution = 100;
+    public int scale = 1;
+    public float displacementStrength = 200.0f;
+    public Material grassMaterial;
+    public Mesh grassMesh;
+    public Texture heightMap;
+
+    public bool updateGrass;
+
+    [Header("Wind")]
+    public float windSpeed = 1.0f;
+    public float frequency = 1.0f;
+    public float windStrength = 1.0f;
+
+    private ComputeShader initializeGrassShader, generateWindShader, cullGrassShader;
+    private ComputeBuffer grassDataBuffer, grassVoteBuffer, grassScanBuffer, groupSumArrayBuffer, scannedGroupSumBuffer, culledGrassOutputBuffer, argsBuffer;
+
+    private ComputeBuffer compactedGrassIndicesBuffer;
+
+    private RenderTexture wind;
+
+    private int numInstances, numGroups;
+
+    private struct GrassData {
+        public Vector4 position;
+        public Vector2 uv;
+        public float displacement;
+    }
+
+    void OnEnable() {
+        numInstances = resolution * scale;
+        numInstances *= numInstances;
+        Debug.Log("NumInstances: " + numInstances.ToString());
+
+        numGroups = numInstances / 128;
+        Debug.Log("NumGroups: " + numGroups.ToString());
+
+        initializeGrassShader = Resources.Load<ComputeShader>("GrassPoint");
+        generateWindShader = Resources.Load<ComputeShader>("WindNoise");
+        cullGrassShader = Resources.Load<ComputeShader>("CullGrass");
+        
+        argsBuffer = new ComputeBuffer(1, 5 * sizeof(uint), ComputeBufferType.IndirectArguments);
+        uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+        // Arguments for drawing mesh.
+        // 0 == number of triangle indices, 1 == population, others are only relevant if drawing submeshes.
+        args[0] = (uint)grassMesh.GetIndexCount(0);
+        args[1] = (uint)0;
+        args[2] = (uint)grassMesh.GetIndexStart(0);
+        args[3] = (uint)grassMesh.GetBaseVertex(0);
+        argsBuffer.SetData(args);
+
+        grassDataBuffer = new ComputeBuffer(numInstances, 4 * 7);
+        grassVoteBuffer = new ComputeBuffer(numInstances, 4);
+        grassScanBuffer = new ComputeBuffer(numInstances, 4);
+        groupSumArrayBuffer = new ComputeBuffer(numGroups, 4);
+        scannedGroupSumBuffer = new ComputeBuffer(numGroups, 4);
+        culledGrassOutputBuffer = new ComputeBuffer(numInstances, 4 * 7);
+
+        compactedGrassIndicesBuffer = new ComputeBuffer(numInstances, 4);
+
+        wind = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+        wind.enableRandomWrite = true;
+        wind.Create();
+
+        updateGrassBuffer();
+        /*
+        uint[] computedata = new uint[numGroups];
+        scannedGroupSumBuffer.GetData(computedata);
+
+        for (int i = 0; i < numGroups; ++i) {
+            Debug.Log(computedata[i]);
+        }*/
+    }
+
+    void updateGrassBuffer() {
+        initializeGrassShader.SetInt("_Dimension", resolution * scale);
+        initializeGrassShader.SetInt("_Scale", scale);
+        initializeGrassShader.SetBuffer(0, "_GrassDataBuffer", grassDataBuffer);
+        initializeGrassShader.SetTexture(0, "_HeightMap", heightMap);
+        initializeGrassShader.SetFloat("_DisplacementStrength", displacementStrength);
+        initializeGrassShader.Dispatch(0, Mathf.CeilToInt((resolution * scale) / 8.0f), Mathf.CeilToInt((resolution * scale) / 8.0f), 1);
+        
+        CullGrass();
+        GenerateWind();
+
+        grassMaterial.SetBuffer("positionBuffer", grassDataBuffer);
+        grassMaterial.SetFloat("_DisplacementStrength", displacementStrength);
+        grassMaterial.SetTexture("_WindTex", wind);
+    }
+
+    void CullGrass() {        
+        Matrix4x4 P = Camera.main.projectionMatrix;
+        Matrix4x4 V = Camera.main.transform.worldToLocalMatrix;
+        Matrix4x4 VP = P * V;
+
+        int threadGroupSizeX = Mathf.CeilToInt(numInstances / 128.0f);
+
+        //Reset Args
+        cullGrassShader.SetBuffer(4, "_ArgsBuffer", argsBuffer);
+        cullGrassShader.Dispatch(4, 1, 1, 1);
+
+        // Vote
+        cullGrassShader.SetMatrix("MATRIX_VP", VP);
+        cullGrassShader.SetBuffer(0, "_GrassDataBuffer", grassDataBuffer);
+        cullGrassShader.SetBuffer(0, "_GrassVoteBuffer", grassVoteBuffer);
+        cullGrassShader.Dispatch(0, Mathf.CeilToInt(numInstances / 128.0f), 1, 1);
+
+        // Scan Instances
+        cullGrassShader.SetBuffer(1, "_GrassVoteBuffer", grassVoteBuffer);
+        cullGrassShader.SetBuffer(1, "_GrassScanBuffer", grassScanBuffer);
+        cullGrassShader.SetBuffer(1, "_GroupSumArray", groupSumArrayBuffer);
+        cullGrassShader.Dispatch(1, threadGroupSizeX, 1, 1);
+
+        // Scan Groups
+        cullGrassShader.SetInt("_NumOfGroups", numGroups);
+        cullGrassShader.SetBuffer(2, "_GroupSumArrayIn", groupSumArrayBuffer);
+        cullGrassShader.SetBuffer(2, "_GroupSumArrayOut", scannedGroupSumBuffer);
+        cullGrassShader.Dispatch(2, Mathf.CeilToInt(numInstances / 1024), 1, 1);
+
+        // Compact
+        cullGrassShader.SetBuffer(3, "_GrassDataBuffer", grassDataBuffer);
+        cullGrassShader.SetBuffer(3, "_GrassVoteBuffer", grassVoteBuffer);
+        cullGrassShader.SetBuffer(3, "_GrassScanBuffer", grassScanBuffer);
+        cullGrassShader.SetBuffer(3, "_ArgsBuffer", argsBuffer);
+        cullGrassShader.SetBuffer(3, "_CulledGrassOutputBuffer", culledGrassOutputBuffer);
+        cullGrassShader.SetBuffer(3, "_GroupSumArray", scannedGroupSumBuffer);
+        cullGrassShader.SetBuffer(3, "_CompactedIndicesBuffer", compactedGrassIndicesBuffer);
+        cullGrassShader.Dispatch(3, threadGroupSizeX, 1, 1);
+    }
+
+    void GenerateWind() {
+        generateWindShader.SetTexture(0, "_WindMap", wind);
+        generateWindShader.SetFloat("_Time", Time.time * windSpeed);
+        generateWindShader.SetFloat("_Frequency", frequency);
+        generateWindShader.SetFloat("_Amplitude", windStrength);
+        generateWindShader.Dispatch(0, Mathf.CeilToInt(wind.width / 8.0f), Mathf.CeilToInt(wind.height / 8.0f), 1);
+    }
+
+    void Update() {      
+        CullGrass();
+        GenerateWind();
+
+        grassMaterial.SetBuffer("positionBuffer", culledGrassOutputBuffer);
+        grassMaterial.SetBuffer("voteBuffer", grassVoteBuffer);
+        grassMaterial.SetFloat("_DisplacementStrength", displacementStrength);
+        grassMaterial.SetTexture("_WindTex", wind);
+
+        Graphics.DrawMeshInstancedIndirect(grassMesh, 0, grassMaterial, new Bounds(Vector3.zero, new Vector3(-500.0f, 200.0f, 500.0f)), argsBuffer);
+
+        if (updateGrass) {
+            updateGrassBuffer();
+            updateGrass = false;
+        }
+    }
+    
+    void OnDisable() {
+        grassDataBuffer.Release();
+        argsBuffer.Release();
+        grassVoteBuffer.Release();
+        grassScanBuffer.Release();
+        culledGrassOutputBuffer.Release();
+        groupSumArrayBuffer.Release();
+        scannedGroupSumBuffer.Release();
+        compactedGrassIndicesBuffer.Release();
+        wind.Release();
+        grassDataBuffer = null;
+        argsBuffer = null;
+        wind = null;
+        scannedGroupSumBuffer = null;
+        grassVoteBuffer = null;
+        grassScanBuffer = null;
+        groupSumArrayBuffer = null;
+        culledGrassOutputBuffer = null;
+        compactedGrassIndicesBuffer = null;
+    }
+}
---
+++ Assets/ModelGrass.shader
@@ -0,0 +1,123 @@
+Shader "Unlit/ModelGrass" {
+    Properties {
+        _Albedo1 ("Albedo 1", Color) = (1, 1, 1)
+        _Albedo2 ("Albedo 2", Color) = (1, 1, 1)
+        _AOColor ("Ambient Occlusion", Color) = (1, 1, 1)
+        _TipColor ("Tip Color", Color) = (1, 1, 1)
+        _WindStrength ("Wind Strength", Range(0.5, 50.0)) = 1
+        _Scale ("Scale", Range(0.0, 2.0)) = 0.0
+        _Droop ("Droop", Range(0.0, 10.0)) = 0.0
+        _CullingBias ("Cull Bias", Range(0.1, 1.0)) = 0.5
+        _LODCutoff ("LOD Cutoff", Range(10.0, 500.0)) = 100
+    }
+
+    SubShader {
+        Cull Off
+        Zwrite On
+
+        Pass {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            #pragma target 4.5
+
+            #include "UnityPBSLighting.cginc"
+            #include "AutoLight.cginc"
+            #include "Random.cginc"
+
+            struct VertexData {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float noiseVal : TEXCOORD1;
+            };
+
+            struct GrassData {
+                float4 position;
+                float2 uv;
+                float displacement;
+            };
+
+            sampler2D _WindTex;
+            float4 _Albedo1, _Albedo2, _AOColor, _TipColor;
+            StructuredBuffer<GrassData> positionBuffer;
+            StructuredBuffer<bool> voteBuffer;
+            float _WindStrength, _CullingBias, _DisplacementStrength, _LODCutoff, _Scale, _Droop;
+
+            float4 RotateAroundYInDegrees (float4 vertex, float degrees) {
+                float alpha = degrees * UNITY_PI / 180.0;
+                float sina, cosa;
+                sincos(alpha, sina, cosa);
+                float2x2 m = float2x2(cosa, -sina, sina, cosa);
+                return float4(mul(m, vertex.xz), vertex.yw).xzyw;
+            }
+            
+            float4 RotateAroundXInDegrees (float4 vertex, float degrees) {
+                float alpha = degrees * UNITY_PI / 180.0;
+                float sina, cosa;
+                sincos(alpha, sina, cosa);
+                float2x2 m = float2x2(cosa, -sina, sina, cosa);
+                return float4(mul(m, vertex.yz), vertex.xw).zxyw;
+            }
+
+            v2f vert (VertexData v, uint instanceID : SV_INSTANCEID) {
+                v2f o;
+                float4 grassPosition = positionBuffer[instanceID].position;
+
+                float idHash = randValue(abs(grassPosition.x * 10000 + grassPosition.y * 100 + grassPosition.z * 0.05f + 2));
+                idHash = randValue(idHash * 100000);
+
+                float4 animationDirection = float4(0.0f, 0.0f, 1.0f, 0.0f);
+                animationDirection = normalize(RotateAroundYInDegrees(animationDirection, idHash * 180.0f));
+
+                float4 localPosition = RotateAroundXInDegrees(v.vertex, 90.0f);
+                localPosition = RotateAroundYInDegrees(localPosition, idHash * 180.0f);
+                localPosition += _Scale * v.uv.y * v.uv.y * v.uv.y;
+                
+                float4 worldUV = float4(positionBuffer[instanceID].uv, 0, 0);
+                
+                float swayVariance = lerp(0.8, 1.0, idHash);
+                float movement = v.uv.y * v.uv.y * v.uv.y * (tex2Dlod(_WindTex, worldUV).r - _Droop);
+                movement *= swayVariance;
+                
+                localPosition.x += movement * animationDirection.x;
+                localPosition.z += movement * animationDirection.y;
+                
+                float4 worldPosition = float4(grassPosition.xyz + localPosition, 1.0f);
+
+                worldPosition.y -= positionBuffer[instanceID].displacement;
+                worldPosition.y *= 1.0f + positionBuffer[instanceID].position.w;
+                worldPosition.y += positionBuffer[instanceID].displacement;
+                
+                o.vertex = UnityObjectToClipPos(worldPosition);
+                /*
+                if (voteBuffer[instanceID] == 0)
+                    o.vertex = 0;
+                */
+                o.uv = v.uv;
+                o.noiseVal = tex2Dlod(_WindTex, worldUV).r;
+
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target {
+                float4 col = lerp(_Albedo1, _Albedo2, i.uv.y);
+                float3 lightDir = _WorldSpaceLightPos0.xyz;
+                float ndotl = DotClamped(lightDir, normalize(float3(0, 1, 0)));
+
+                float4 ao = lerp(_AOColor, 1.0f, i.uv.y);
+                float4 tip = lerp(0.0f, _TipColor, i.uv.y * i.uv.y * i.uv.y);
+
+                //return i.noiseVal;
+                return (col + tip) * ndotl * ao;
+            }
+
+            ENDCG
+        }
+    }
+}
