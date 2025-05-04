71899bbc7bb7f0f4b1485e02b7e734629c6e6650
Michael Sturm <michael.c.sturm@gmail.com>
2025-05-04 17:15:05 -0500

    doing allot of fxing and remaking visuals

--- Assets/BeatSaberHitbox.cs
+++ Assets/BeatSaberHitbox.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BeatSaberHitbox : Hitbox
 {
     public ParticleSystem system;
     //private bool hit = false;
+    bool once = true;
+    public Animator animator;
     void Update()
     {
-        if (hit)
+        if (hit && once)
         {
             var emitParams = new ParticleSystem.EmitParams();
             emitParams.startColor = Color.red;
             emitParams.startSize = 0.2f;
             system.Emit(emitParams, 10);
             system.Play(); 
+            once = false;
+            animator.Play("MakeSmaller on hit");
        }
     }
 
     void OnTriggerEnter(Collider other)
     {
+        if (other.tag != "BeatSaberBox")
+        {
+            hit = true;
+            Debug.Log("ayo it hit");
+        }
         
-        hit = true;
-        Debug.Log("ayo it hit");
     }
 }
