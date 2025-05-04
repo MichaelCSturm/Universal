b313a5197fee5cafd8ff167da6c89660bdbbbf38
Michael Sturm <michael.c.sturm@gmail.com>
2025-05-03 23:27:22 -0500

    ball spawner spawning too many balls

---
+++ Assets/BallSpawner.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.FilePathAttribute;
+
+
+public class BallSpawner : MonoBehaviour
+{
+    public float waitTime;
+    bool quit = false;
+    public GameObject Ball;
+    public GameObject Spawner;
+    // Start is called before the first frame update
+    public void SpawnBall()
+    {
+        Vector3 Location = Spawner.transform.position;
+        Instantiate(Ball, Location, Quaternion.identity);
+        //print("Spawning");
+    }
+    void Update()
+    {
+        StartCoroutine(waiter());
+
+    }
+
+
+    IEnumerator waiter()
+    {
+
+
+       
+        
+        yield return wait(waitTime);
+        SpawnBall();
+        yield return wait(waitTime);
+    }
+
+    IEnumerator wait(float waitTime)
+    {
+        float counter = 0;
+
+        while (counter < waitTime)
+        {
+            //Increment Timer until counter >= waitTime
+            counter += Time.deltaTime;
+            //Debug.Log("We have waited for: " + counter + " seconds");
+            if (quit)
+            {
+                //Quit function
+                yield break;
+            }
+            //Wait for a frame so that Unity doesn't freeze
+            yield return null;
+        }
+    }
+}
