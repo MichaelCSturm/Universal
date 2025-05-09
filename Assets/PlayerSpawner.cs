c91d3bf85ed1fd4e1c65927e59c1974e9b86c530
Michael Sturm <michael.c.sturm@gmail.com>
2025-05-09 00:48:46 -0500

    Fixed Construction

--- Assets/PlayerSpawner.cs
+++ Assets/PlayerSpawner.cs
@@ -7,7 +7,13 @@ public class PlayerSpawner : MonoBehaviour
     public GameObject playerFinal;
     GameObject player;
     // Start is called before the first frame update
-    void Awake()
+    IEnumerator Start()
+    {
+        yield return null; // Wait one frame to ensure other scripts run
+        SpawnPlayer();
+    }
+
+    void SpawnPlayer()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         if (player == null)
@@ -17,11 +23,12 @@ public class PlayerSpawner : MonoBehaviour
         }
         else
         {
+            player.transform.SetParent(null);
             player.transform.position = transform.position;
             player.transform.rotation = transform.rotation;
             player.transform.localScale = transform.localScale;
         }
     }
 
-    
+
 }
